02aabe87503c38467212240b440e412e08f42537
Eric Wu <ericwu694@gmail.com>
2024-07-29 01:35:49 -0400

    React Router Setup

--- algoarchive/src/App.tsx
+++ algoarchive/src/App.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import HomePage from './pages/HomePage'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index element/> 
+  )
+)
 
 const App = () => {
   return (
