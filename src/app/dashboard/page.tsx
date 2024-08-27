4f41259efd32d0fb77951f1a373ccba5f5d924e3
Eric Wu <ericwu694@gmail.com>
2024-08-27 11:24:05 -0400

    Reverted Changes back to regular Navbar
    Reverted from sidenav to previous nav
    
    Added in Analytics tab to previous nav

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,13 +5,8 @@ import Nav from '@/src/components/Dashboard/Navbar';
 
 export default function page(){
   return (
-    <main className="algoarchive bg-[#1a1a1a]">
+    <main className="algoarchive">
         <Nav />
-        <div className="lg:ml-60 p-6 h-ful"> 
-          <h1 className="text-white">Spent the whole day figuring this stupid sidebar out!</h1>
-          <h2 className="text-white">Warning: This is not gonna look very pretty on mobile unless Nora clutches up and removes all the margins and padding to make it responsive...</h2>
-          <h2 className="text-white">I should've slept earlier</h2>
-        </div>
     </main>
   )
 }
