b90fdaf66ca36a6f705c1b8bc13331164bf013a2
Eric Wu <ericwu694@gmail.com>
2024-09-01 23:25:54 -0400

    Update Hero.tsx
    Made userName bolder to make it stick out more

--- src/components/Dashboard/Hero.tsx
+++ src/components/Dashboard/Hero.tsx
@@ -5,7 +5,7 @@ import Submissions from './Submissions';
 const Hero = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-8 px-4">
-      <h1 className="font-bold text-white text-4xl">Hey <span className="text-algo-yellow">userName!</span></h1>
+      <h1 className="font-bold text-white text-4xl">Hey <span className="text-algo-yellow font-extrabold ">userName!</span></h1>
       <h2 className="font-semibold text-white text-2xl">Check your studying progress below</h2>
       <Searchbar />
       <Submissions />
