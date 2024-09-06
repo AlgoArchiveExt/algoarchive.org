e7a500febbe4aa0783c58c4bbf0d72f341828a7e
Eric Wu <ericwu694@gmail.com>
2024-09-05 22:54:37 -0400

    Update HeroSection.tsx
    Changed HeroSection button to take users to the download link on chrome and updated UI, resolved some linting errors

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -7,33 +7,28 @@ export default function HeroSection() {
       <Card className="p-8 max-w-2xl mx-auto bg-transparent border-none shadow-none">
         <CardBody>
           <h1 className="text-4xl font-bold text-white mb-4">
-          Effortlessly synchronize your solutions to GitHub with
+            Effortlessly synchronize your solutions to GitHub with
             <span className="bg-gradient-to-r from-[#EE9B01] to-[#f7b801] text-transparent bg-clip-text">
               {" "} AlgoArchive
             </span>
           </h1>
           <p className="text-lg text-gray-400 mb-8">
-          Easily sync your LeetCode solutions with GitHub and showcase your programming journey.
+            Easily sync your LeetCode solutions with GitHub and showcase your programming journey. 
           </p>
-          <div className="flex justify-center space-x-4">
+          <div className="flex flex-col items-center space-y-4">
             <Button
-              href="/waitlist"
+              href="https://chromewebstore.google.com/detail/algoarchive/anjcgdjflkjlhaopbilibnbfpialdbpl"
               as={Link}
               target="_blank"
               showAnchorIcon
               variant="ghost"
               className="bg-gradient-to-r from-[#EE9B01] to-[#f7b801] text-white px-6 py-2 rounded-full hover:text-black"
             >
-              Join the waitlist
+              Download
             </Button>
-            {/* <Button
-              href="#"
-              as={Link}
-              variant="solid"
-              className="bg-[#825534] text-white px-6 py-2 rounded-full"
-            >
-              Login
-            </Button> */}
+            <p className="text-base bg-gradient-to-r from-[#EE9B01] to-[#f7b801] text-transparent bg-clip-text">            
+              Get Started by downloading the extension!
+            </p>
           </div>
         </CardBody>
       </Card>
