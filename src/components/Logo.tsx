921ea0b6d9ea41e86b8bcb539d31fd02ef98c888
Eric Wu <ericwu694@gmail.com>
2024-09-07 15:13:33 -0400

    Update Logo.tsx
    Fixed Logo Spacing issue, Logo should appear inline together now

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ import logo from '/public/assets/AlgoArchive.png';
 export default function Logo() {
   // After receiving Anthony's API to user's submissions, will implement logic to list their submissions. The formatting and how this will look will have to be discussed
   return (
-    <Link color="foreground" href="/">
+    <Link color="foreground" href="/" className="flex items-center">
       <Image src={logo.src} alt="Logo" width="18" height="18" />
       <p className="font-bold text-white">lgoArchive</p>
     </Link>
