8829324d4baeb372dc7edd61c7136ee7c68ee0af
lausan3 <lauanthony4455@gmail.com>
2024-09-08 15:40:04 -0400

    🧪 test(auth): remove js-cookie and add domain name based on environment

--- src/actions/UserProfile.ts
+++ src/actions/UserProfile.ts
@@ -1,21 +1,21 @@
 'use server';
 
-import Cookies from 'js-cookie';
+import { cookies } from 'next/headers';
 
 export async function writeSignedInHeader(githubAccessToken: string | null) {
-  const cookie = Cookies;
+  const cookie = cookies();
 
   if (!githubAccessToken) {
-    cookie.remove('signed_in');
+    cookie.delete('signed_in');
 
     return;
   }
 
-  cookie.set('signed_in', '', {
+  cookie.set('signed_in', 'true', {
     httpOnly: true,
     secure: true,
     sameSite: 'lax',
     maxAge: 60 * 60 * 24 * 7, // 1 week
-    domain: 'algoarchive.org',
+    domain: process.env.COOKIE_DOMAIN || '.algoarchive.org',
   });
 }
